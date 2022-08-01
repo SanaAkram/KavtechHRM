82285285a45e05f6c5b77cdf9f62ca0148733acb
Skillsquad <sanaakram582@gmail.com>
2022-08-01 12:10:57 +0500

    front/backend

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,4 +5,4 @@ from .models import User
 # Register your models here.
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['id', 'first_name', 'last_name', 'username', 'email', 'password']
+    list_display = ['id', 'username', 'email', 'password']
--- accounts/models.py
+++ accounts/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 
 class User(models.Model):
-    first_name = models.CharField(max_length=10)
-    last_name = models.CharField(max_length=10)
+    # first_name = models.CharField(max_length=10)
+    # last_name = models.CharField(max_length=10)
     username = models.CharField(max_length=40)
     email = models.EmailField(max_length=30)
     password = models.CharField(max_length=50)
